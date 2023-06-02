ec59e1d4a9be98919917e54cfb48ec8f8f85743b
Javi <120780139+Jarv1er@users.noreply.github.com>
2023-06-02 21:56:19 +0200

    Add files via upload

---
+++ 1_tests.py
@@ -0,0 +1,52 @@
+# for i in range(10):
+#     print("El valor de i es actualmente", i)
+
+# for i in range(2, 8):
+#     print("El valor de i es actualmente", i)
+
+# power = 1
+# for expo in range(16):
+#     print("2 a la potencia de", expo, "es", power)
+#     power *= 2
+
+counter = 5
+while counter > 2:
+    print(counter)
+    counter -= 1
+
+word = "Python"
+for letter in word:
+    print(letter, end="*")
+
+for i in range(1, 10):
+    if i % 2 == 0:
+        print(i)
+
+
+text = "OpenEDG Python Institute"
+for letter in text:
+    if letter == "P":
+        break
+    print(letter, end="")
+
+text2 = "pyxpyxpyx"
+for letra in text2:
+    if letra == "x":
+        continue
+    print(letra, end="")
+
+
+n = 0
+
+while n != 3:
+    print(n)
+    n += 1
+else:
+    print(n, "else")
+
+print()
+
+for i in range(0, 3):
+    print(i)
+else:
+    print(i, "else")
---
+++ 2_datos_var.py
@@ -0,0 +1,24 @@
+''' 
+La tarea es preparar un código simple para evaluar o encontrar el tiempo final de un periodo de tiempo dado, 
+expresándolo en horas y minutos. Las horas van de 0 a 23 y los minutos de 0 a 59. 
+El resultado debe ser mostrado en la consola.
+
+Por ejemplo, si el evento comienza a las 12:17 y dura 59 minutos, terminará a las 13:16.
+No te preocupes si tu código no es perfecto, está bien si acepta una hora invalida, 
+lo más importante es que el código produzca una salida correcta acorde a la entrada dada.
+
+Prueba el código cuidadosamente. Pista: utilizar el operador % puede ser clave para el éxito.
+'''
+
+hour = int(input("Hora de inicio (horas): "))
+mins = int(input("Minuto de inicio (minutos): "))
+dura = int(input("Duración del evento (minutos): "))
+mins = mins + dura # encuentra el número total de minutos
+hour = hour + mins // 60 # encuentra el número de horas ocultas en los minutos y actualiza las horas
+mins = mins % 60 # corrige los minutos para que estén en un rango de (0..59)
+hour = hour % 24 # corrige las horas para que estén en un rango de (0..23) 
+print(hour, ":", mins, sep='')
+
+
+print(mins % 60) 
+print(hour % 24)
